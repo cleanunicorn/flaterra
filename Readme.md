dbd39f3f43a6c740a788c27cb81925f88106f3c0
Daniel Luca <lucadanielcostin@gmail.com>
2019-02-11 20:08:11 +0200

    Add a first version before packing it for pypi

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Flat Earth
+
+A way to make Solidity files flat like the earth.
+
---
+++ flaterra.py
@@ -0,0 +1,93 @@
+import re
+import argparse
+import logging
+
+# Save what files were already imported
+imported_files = {}
+
+
+def flat_file(path, file, main=False):
+    logging.info("Reading file {path}/{file}".format(path=path, file=file))
+
+    flat_source = ""
+    global imported_files
+
+    with open("{path}/{file}".format(path=path, file=file)) as f:
+        read_data = f.readlines()
+
+    # Match for pragma at level >0
+    # ex: pragma solidity 0.5.0;
+    # ex: pragma experimental ABIEncoderV2;
+    pragma_regex = r"\s*pragma\s*[solidity|experimental].*;"
+
+    for l in read_data:
+        # Skip other pragma definitions for included files
+        if main is False:
+            pragma_match = re.search(pragma_regex, l)
+            if pragma_match is not None:
+                continue
+
+        # Import files that are referenced
+        # ex: import "./contract.sol";
+        # ex: import {Contract, Contract2} from "./contracts.sol";
+        import_match = re.findall(
+            r"^\s*import\s+|(?!{.*}\s*from\s*)\"(.*)\"\s*;\s*$", l
+        )
+        if len(import_match) == 2:
+            imported_file = import_match[1]
+
+            if (imported_file is not None) and (
+                imported_files.get(imported_file) is None
+            ):
+                imported_files[imported_file] = True
+                logging.info("Importing file {file}".format(file=imported_file))
+                flat_source += flat_file(path=path, file=imported_file)
+                flat_source += "\n"
+            else:
+                logging.debug("Skipping file {file}".format(file=imported_file))
+
+            # Do not add the import clause
+            continue
+
+        # Add line
+        flat_source += l
+
+    return flat_source
+
+
+def main():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "--contractsdir", help="Source directory of contracts", default="./contracts/"
+    )
+    parser.add_argument("--mainsol", help="Main source Solidity file")
+    parser.add_argument("--outputsol", help="Output flattened Solidity file.")
+    parser.add_argument("--verbose", "-v", action="count", default=0, help="Show details")
+    args = parser.parse_args()
+
+    contracts_dir = args.contractsdir
+    main_sol = args.mainsol
+    output_sol = args.outputsol
+    verbose = args.verbose
+
+    # Set verbosity
+    logging.basicConfig(level=logging.INFO)
+    if verbose >= 1:
+        logging.basicConfig(level=logging.DEBUG)
+
+    source = flat_file(path=contracts_dir, file=main_sol, main=True)
+
+    if not output_sol:
+        output_sol = main_sol.split(".")
+        output_sol.pop()
+        output_sol[-1] += "_flat"
+        output_sol.append("sol")
+        output_sol = ".".join(output_sol)
+
+    logging.info("Writing flattened file {file}".format(file=output_sol))
+    with open(output_sol, "w") as f:
+        f.write(source)
+
+
+if __name__ == "__main__":
+    main()
